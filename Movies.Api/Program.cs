0e3f24ec922c6c097e34e54ec27968f75b644c33
Prem <mprem39@gmail.com>
2026-09-13 18:17:23 +0530

    Added Heath check endpoint

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Movies.Api.Auth;
+using Movies.Api.Health;
 using Movies.Api.Mapping;
 using Movies.Api.Swagger;
 using Movies.Appilication;
@@ -61,6 +62,9 @@ builder.Services.AddApiVersioning(options=>
 builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 builder.Services.AddSwaggerGen(x => x.OperationFilter<SwaggerDefaultValues>());
 
+builder.Services.AddHealthChecks()
+    .AddCheck<DatabaseHealthCheck>(DatabaseHealthCheck.Name);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -80,6 +84,7 @@ if (app.Environment.IsDevelopment())
     }
 }
 
+app.MapHealthChecks("_health");
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
